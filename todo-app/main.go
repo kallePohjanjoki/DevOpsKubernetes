8eba5e384f46e0c32093ebfa281ddd00b71970cb
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2025-12-20 00:06:56 +0200

    Complete exercise 1.2

---
+++ todo-app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Todo App - Hello World!")
+	})
+
+	fmt.Printf("Server started in port %s\n", port)
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
